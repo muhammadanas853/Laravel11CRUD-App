5e6871c1f4600bc97ccb8b671fb20a0ba8dd706e
muhammadanas853 <muhammadanas851@gmail.com>
2024-08-22 23:06:46 +0500

    First commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::controller(ProductController::class)->group(function(){
+    Route::get('/products','index')->name('products.index');
+    Route::get('/products/create','create')->name('products.create');
+    Route::post('/products','store')->name('products.store');
+    Route::get('/products/{product}/edit','edit')->name('products.edit');
+    Route::put('/products/{product}','update')->name('products.update');
+    Route::delete('/products/{product}','destory')->name('products.destory');    
+});
